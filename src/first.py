1ba4879859e787f7b8bc1167f7936f8f3531abbd
Wade <wade5141000@outlook.com>
2020-12-28 01:03:34 +0800

    test

--- src/first.py
+++ src/first.py
@@ -1,1 +1,12 @@
-print('hello world')
+import csv
+
+with open('src/data/USvideos.csv', newline='', encoding='utf-8') as csvfile:
+
+    rows = csv.reader(csvfile)
+    headers = next(rows)
+    row1 = next(rows)
+    print(headers)
+    print(row1[7])
+
+    
+    
