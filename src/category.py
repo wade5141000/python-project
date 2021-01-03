9a28802a0a5201eab6fba820735d6edd2214e20f
Wade <wade5141000@outlook.com>
2021-01-03 21:38:01 +0800

    ipynb

---
+++ src/category.py
@@ -0,0 +1,14 @@
+class Category():
+
+
+    def ppp(self, x):
+        print(x)
+
+
+
+
+cat = Category()
+
+cat.ppp(123)
+
+# pd.read_json('data/US_category_id.json')['items'][0]
--- src/first.py
+++ src/first.py
@@ -1,12 +1,18 @@
-import csv
+import json
 
-with open('src/data/USvideos.csv', newline='', encoding='utf-8') as csvfile:
+file = 'src/data/US_category_id.json'
+with open(file, 'r') as obj:
+    data = json.load(obj)
 
-    rows = csv.reader(csvfile)
-    headers = next(rows)
-    row1 = next(rows)
-    print(headers)
-    print(row1[7])
+id=19
 
+for x in data['items']:
+    if(int(x['id']) == id):
+        print(x['snippet']['title'])
     
-    
+
+# 轉換分析後寫出檔案
+
+# 畫圖展示
+
+# data frame 合併統計
